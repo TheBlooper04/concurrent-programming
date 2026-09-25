7fa942d7e0caea10a8a1dc9881675d532ce68ccc
Ignacio Fernandez Urreta <76232604+TheBlooper04@users.noreply.github.com>
2026-09-25 17:17:20 +0200

    Add Peterson's algorithm implemented in Python

---
+++ 01-critical-section/perterson-algo/peterson-algo.py
@@ -0,0 +1,40 @@
+import threading
+
+MAX_VALUE = 1_000_000
+NUM_THREADS = 2
+
+want_cs = [False, False]
+turn = 0
+counter = 0
+
+def count(tid: int) -> None:
+    global want_cs, turn, counter
+    for _ in range(MAX_VALUE // NUM_THREADS):
+        # Entry protocol
+        want_cs[tid] = True
+        other_thread_id = (tid + 1) % NUM_THREADS
+        turn = other_thread_id
+        while want_cs[other_thread_id] == True and turn == other_thread_id:
+            pass
+
+        # Critical Section
+        counter += 1
+
+        # Exit protocol
+        want_cs[tid] = False
+
+def main() -> None:
+    threads = []
+
+    for i in range(NUM_THREADS):
+        t = threading.Thread(target=count, args=(i,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print(f"Counter value: {counter} | Expected value: {MAX_VALUE}")
+
+if __name__ == "__main__":
+    main()
